224f98b057c1f8b4212877816f5c91851208d1e0
Garrappachc <michal@garapich.pl>
2012-09-15 15:06:51 +0200

    Cmake cleanup.

--- cmake-modules/Packages.cmake
+++ cmake-modules/Packages.cmake
@@ -16,18 +16,8 @@ set (CPACK_PACKAGE_DESCRIPTION "${vatsinator_DESCRIPTION}")
 
 set (CPACK_DEBIAN_PACKAGE_ARCHITECTURE "amd64")
 set (CPACK_DEBIAN_PACKAGE_DEPENDS "libqt4-core (>=4.7), libqt4-gui (>=4.7), libqt4-opengl (>=4.7), libqt4-network (>=4.7)")
-set (CPACK_DEBIAN_BUILD_DEPENDS "debhelper, cmake, libqt4-opengl-dev (>=4.7), qt4-qmake (>=4.7)")
-set (CPACK_DEBIAN_PACKAGE_SECTION "web")
-set (CPACK_DEBIAN_CHANGELOG "  * Initial release")
 set (CPACK_DEBIAN_PACKAGE_PRIORITY extra)
 set (CPACK_DEBIAN_PACKAGE_SECTION web)
-set (CPACK_DEBIAN_CMAKE_OPTIONS "")
-set (CPACK_DEBIAN_PACKAGE_SUGGESTS "metar")
-
-set (DPUT_HOST ppa:michal-k93/vatsinator)
-
-set (CPACK_DEBIAN_DISTRIBUTION_RELEASES precise)
-set (CPACK_DEBIAN_DISTRIBUTION_NAME ubuntu)
 
 set (CPACK_RPM_PACKAGE_ARCHITECTURE "x86_64")
 set (CPACK_RPM_PACKAGE_DESCRIPTION "The simple Vatsim monitor that shows logged-in pilots, ATCs and provides some information useful during flights.")
@@ -36,5 +26,4 @@ set (CPACK_RPM_PACKAGE_GROUP "Applications/Internet")
 
 
 include (CPack)
-include (DebSourcePPA)
 
