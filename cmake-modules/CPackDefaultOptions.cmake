1ea4d6287ffcc9ae3cda11b876e592192b426153
Garrappachc <michal@garapich.pl>
2012-09-18 17:21:29 +0200

    Several small changes. - Added COPYING file to install sheet; - added GenericName[pl] and Comment[pl] to .desktop file; - CPack cleanup.

---
+++ cmake-modules/CPackDefaultOptions.cmake
@@ -0,0 +1,9 @@
+set (CPACK_PACKAGE_CONTACT "Michał Garapich michal@garapich.pl")
+set (CPACK_PACKAGE_NAME "Vatsinator")
+set (CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYING")
+
+set (CPACK_PACKAGE_DESCRIPTION_SUMMARY "Vatsim monitor")
+set (CPACK_PACKAGE_FILE_NAME "vatsinator-${vatsinator_VERSION}")
+set (CPACK_PACKAGE_EXECUTABLES "vatsinator;Vatsinator")
+set (CPACK_PACKAGE_INSTALL_DIRECTORY "Vatsinator")
+set (CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/resources/vatsinator-24.png")
--- cmake-modules/Packaging-win32.cmake
+++ cmake-modules/Packaging-win32.cmake
@@ -1,16 +1,8 @@
-string (REPLACE "_" "." CPACK_PACKAGE_VERSION ${vatsinator_VERSION})
-#set (CPACK_PACKAGE_VERSION "")
+#string (REPLACE "_" "." CPACK_PACKAGE_VERSION ${vatsinator_VERSION})
 
 set (CPACK_GENERATOR "NSIS")
-set (CPACK_PACKAGE_CONTACT "Michał Garapich michal@garapich.pl")
-set (CPACK_PACKAGE_NAME "Vatsinator")
-set (CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/COPYING")
 
-set (CPACK_PACKAGE_DESCRIPTION_SUMMARY "Vatsim monitor")
-set (CPACK_PACKAGE_FILE_NAME "vatsinator-${vatsinator_VERSION}")
-set (CPACK_PACKAGE_EXECUTABLES "vatsinator;Vatsinator")
-set (CPACK_PACKAGE_INSTALL_DIRECTORY "Vatsinator")
-set (CPACK_PACKAGE_ICON "${CMAKE_CURRENT_SOURCE_DIR}/resources/vatsinator-24.png")
+include (CPackDefaultOptions)
 
 set (CPACK_NSIS_EXECUTABLES_DIRECTORY ".")
 
