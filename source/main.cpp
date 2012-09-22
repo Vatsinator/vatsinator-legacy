25891765fe7e25508ec422c484d0ad8f6c3b4619
Garrappachc <michal@garapich.pl>
2012-09-22 21:15:21 +0200

    MacOS packaging in progress.

--- source/main.cpp
+++ source/main.cpp
@@ -1,21 +1,14 @@
 #include "vatsinatorapplication.h"
 
 #ifdef Q_OS_DARWIN
-#include "CoreFoundation/CoreFoundation.h"
+#import <Foundation/Foundation.h>
 #endif
 
 int main(int argc, char** argv) {
   
 #ifdef Q_OS_DARWIN
-  CFBundleRef mainBundle = CFBundleGetMainBundle();
-  CFURLRef resourcesURL = CFBundleCopyResourcesDirectoryURL(mainBundle);
-  char path[PATH_MAX];
-  if (!CFURLGetFileSystemRepresentation(resourcesURL, TRUE, (UInt8 *)path, PATH_MAX)) {
-        exit(1);
-  }
-  CFRelease(resourcesURL);
-  
-  chdir(path);
+  NSString *resourcePath = [[NSBundle mainBundle] resourcePath];
+  [[NSFileManager defaultManager] changeCurrentDirectoryPath:resourcePath];
 #endif
   
   return VatsinatorApplication(argc, argv).exec();
