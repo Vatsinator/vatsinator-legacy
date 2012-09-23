d99be1200f4754b67feda08a8a38b99d7d9c6126
Michał Garapich <michal@garapich.pl>
2012-09-24 00:29:05 +0200

    Dput script development.

--- dist/dput.sh
+++ dist/dput.sh
@@ -1,4 +1,45 @@
-#!/bin/bash
+#!/bin/sh -e
 
-echo "Foo"
+#
+# This script is under construction.
+#
+
+dist=$1
+branch=$2
+
+if [ -z "$dist" ]; then
+	echo "Usage: $0 <dist> [branch]"
+	echo "Example: $0 precise master"
+	exit 1
+fi
+
+vatsinator_ppa=ppa:michal-k93/vatsinator
+vatsinator_git=git://github.com/Garrappachc/Vatsinator.git
+
+tmpdir=`pwd`/work
+builddir=vatsinator
+
+echo "Temporary directory: $tmpdir"
+echo "Build directory: $tmpdir/$builddir"
+
+echo "Downloading source..."
+
+# prepare
+mkdir -p $tmpdir
+
+# clone source code
+cd $tmpdir
+if [ -z "$branch" ]; then
+	git clone $vatsinator_git $builddir
+else
+	git clone $vatsinator_git -b $branch $builddir
+fi
+
+echo "Cleaning up..."
+
+# cleanup
+cd $tmpdir/..
+rm -rf $tmpdir
+
+echo "Done."
 
