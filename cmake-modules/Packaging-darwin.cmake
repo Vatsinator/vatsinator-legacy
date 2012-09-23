f2dcde91889f94506d52c3d3c8ba6bf860e9655c
Garrappachc <michal@garapich.pl>
2012-09-23 16:48:14 +0200

    MacOS packaging in progress.

--- cmake-modules/Packaging-darwin.cmake
+++ cmake-modules/Packaging-darwin.cmake
@@ -7,23 +7,17 @@ if (APPLE AND CMAKE_INSTALL_PREFIX MATCHES "/usr/local")
 endif (APPLE AND CMAKE_INSTALL_PREFIX MATCHES "/usr/local")
 
 string (REPLACE "_" "-" CPACK_PACKAGE_VERSION ${vatsinator_VERSION})
-# 
-set (CPACK_DMG_FORMAT "UDBZ")
-set (CPACK_DMG_VOLUME_NAME "Vatsinator")
-set (CPACK_PACKAGE_FILE_NAME "${CMAKE_PROJECT_NAME}-${CPACK_PACKAGE_VERSION}")
-set (CPACK_BUNDLE_STARTUP_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/vatsinator")
+
+set (CPACK_COMPONENT_LIBRARIES_HIDDEN TRUE)
+
+set (MACOSX_BUNDLE_BUNDLE_NAME Vatsinator)
+set (MACOSX_BUNDLE_BUNDLE_VERSION ${vatsinator_VERSION})
+set (MACOSX_BUNDLE_SHORT_VERSION_STRING ${vatsinator_VERSION})
+set (MACOSX_BUNDLE_LONG_VERSION_STRING "Version ${vatsinator_VERSION}")
 
 set (CPACK_PACKAGE_EXECUTABLES "vatsinator")
-
-set (MACOSX_BUNDLE_ICON_FILE vatsinator.icns)
-set (CPACK_PACKAGE_ICON ${CMAKE_CURRENT_SOURCE_DIR}/source/ui/resources/vatsinator.icns)
-set (CPACK_BUNDLE_NAME "Vatsinator")
+set (CPACK_PACKAGE_FILE_NAME "vatsinator")
 set (CPACK_BUNDLE_ICON ${CMAKE_CURRENT_SOURCE_DIR}/source/ui/resources/vatsinator.icns)
 set (CPACK_BUNDLE_PLIST "Info.plist")
-
-set_source_files_properties (
-    ${CMAKE_CURRENT_SOURCE_DIR}/source/ui/resources/vatsinator.icns
-    PROPERTIES MACOSX_PACKAGE_LOCATION Resources
-)
 
 include (CPack)
