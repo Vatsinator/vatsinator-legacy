0c44c5df6f0a13b14645ae7e0a3c576de9ade09d
Garrappachc <michal@garapich.pl>
2012-10-12 23:16:10 +0200

    Desktop file update so it passes QA validation.

--- dist/vatsinator-desktop.sh
+++ dist/vatsinator-desktop.sh
@@ -9,7 +9,7 @@ GenericName[pl]=Monitor sieci Vatsim
 Comment=A simple Vatsim monitor
 Comment[pl]=Monitor sieci Vatsim
 TryExec=$2/bin/vatsinator
-Exec=$2/bin/vatsinator &
-Categories=Application;Network;Qt;
+Exec=$2/bin/vatsinator
+Categories=Network;Qt;
 Icon=vatsinator
 EOF
