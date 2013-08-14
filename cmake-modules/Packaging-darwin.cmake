525083b0589f1fff98537f42dc3d343b6fb83bad
Garrappachc <michal@garapich.pl>
2013-08-14 23:57:05 +0200

    MacOS X fixes: - gui - build scripts

--- cmake-modules/Packaging-darwin.cmake
+++ cmake-modules/Packaging-darwin.cmake
@@ -1,3 +1,5 @@
+include (CPackDefaultOptions)
+
 if (CMAKE_INSTALL_PREFIX MATCHES "/usr/local")
        set (CMAKE_INSTALL_PREFIX "/Applications")
 endif (CMAKE_INSTALL_PREFIX MATCHES "/usr/local")
@@ -15,8 +17,6 @@ set (MACOSX_BUNDLE_BUNDLE_NAME "Vatsinator")
 
 set (MACOSX_BUNDLE_RESOURCES "${CMAKE_CURRENT_BINARY_DIR}/${vatsinator_BUNDLE}/Contents/Resources")
 set (MACOSX_BUNDLE_ICON "${PROJECT_SOURCE_DIR}/dist/${MACOSX_BUNDLE_ICON_FILE}")
-#execute_process (COMMAND ${CMAKE_COMMAND} -E make_directory ${MACOSX_BUNDLE_RESOURCES})
-#execute_process (COMMAND ${CMAKE_COMMAND} -E copy_if_different ${MACOSX_BUNDLE_ICON} ${MACOSX_BUNDLE_RESOURCES})
 
 set (CPACK_GENERATOR "DragNDrop")
 set (CPACK_DMG_FORMAT "UDBZ")
--- cmake-modules/VatsinatorInstall.cmake
+++ cmake-modules/VatsinatorInstall.cmake
@@ -6,10 +6,10 @@ macro (vatsinator_install INSTALL_FILES INSTALL_DIR)
         install (FILES ${INSTALL_FILES}
                  DESTINATION .${INSTALL_DIR})
     elseif (APPLE)
-        string (REPLACE "/" "_" INSTALL_FILES ${INSTALL_FILES})
+        string (REPLACE "/" "_" DIR_NAME ${INSTALL_DIR})
         foreach (f ${INSTALL_FILES})
-          set (vatsinator_BUNDLE_RESOURCES${INSTALL_DIR}
-               ${vatsinator_BUNDLE_RESOURCES${INSTALL_DIR}}  ${CMAKE_CURRENT_SOURCE_DIR}/${f})
+          set (vatsinator_BUNDLE_RESOURCES${DIR_NAME}
+               ${vatsinator_BUNDLE_RESOURCES${DIR_NAME}}  ${CMAKE_CURRENT_SOURCE_DIR}/${f})
         endforeach (f ${INSTALL_FILES})
     endif (LINUX)
 endmacro (vatsinator_install)
