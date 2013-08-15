ef245dc602aaccee8e49cef9c1f8fe242c337c74
Garrappachc <michal@garapich.pl>
2013-08-15 01:16:08 -0700

    CMake fixes

--- cmake-modules/VatsinatorInstall.cmake
+++ cmake-modules/VatsinatorInstall.cmake
@@ -6,10 +6,14 @@ macro (vatsinator_install INSTALL_FILES INSTALL_DIR)
         install (FILES ${INSTALL_FILES}
                  DESTINATION .${INSTALL_DIR})
     elseif (APPLE)
-        string (REPLACE "/" "_" DIR_NAME ${INSTALL_DIR})
+        string (REPLACE "/" "_" DIR_NAME "${INSTALL_DIR}")
         foreach (f ${INSTALL_FILES})
           set (vatsinator_BUNDLE_RESOURCES${DIR_NAME}
                ${vatsinator_BUNDLE_RESOURCES${DIR_NAME}}  ${CMAKE_CURRENT_SOURCE_DIR}/${f})
+          if (NOT ${CMAKE_CURRENT_SOURCE_DIR} STREQUAL ${CMAKE_SOURCE_DIR})
+            set (vatsinator_BUNDLE_RESOURCES${DIR_NAME}
+              ${vatsinator_BUNDLE_RESOURCES${DIR_NAME}} PARENT_SCOPE)
+          endif (NOT ${CMAKE_CURRENT_SOURCE_DIR} STREQUAL ${CMAKE_SOURCE_DIR})
         endforeach (f ${INSTALL_FILES})
     endif (LINUX)
 endmacro (vatsinator_install)
