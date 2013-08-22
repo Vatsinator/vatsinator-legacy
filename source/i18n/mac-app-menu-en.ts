90542a7bc9eb72f7689f2e2986fb374b64c04877
Garrappachc <michal@garapich.pl>
2013-08-21 22:31:54 -0700

    Added translation source for mac-app-menu

---
+++ source/i18n/mac-app-menu-en.ts
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8">
+<!DOCTYPE TS>
+<TS version="2.0" language="en_GB" sourcelanguage="en_GB">
+<context>
+    <name>MAC_APPLICATION_MENU</name>
+    <message>
+        <source>Services</source>
+        <translation>Services</source>
+    </message>
+</context>
+</TS>
