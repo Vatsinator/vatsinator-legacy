8baadc3f038e4ab8821aa02ad1bdc13effd4a018
Garrappachc <michal@garapich.pl>
2013-09-22 17:14:02 +0200

    Added polish translation

--- source/i18n/mac-app-menu-pl.ts
+++ source/i18n/mac-app-menu-pl.ts
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="en_GB" sourcelanguage="en_GB">
+<TS version="2.0" language="pl" sourcelanguage="en_GB">
 <context>
     <name>MAC_APPLICATION_MENU</name>
     <message>
         <source>Services</source>
-        <translation>Services</translation>
+        <translation>Usługi</translation>
     </message>
 </context>
 </TS>
