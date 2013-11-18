c6d7345e419e44051963f09ad3a9c2908085cd0b
Michał Garapich <michal@garapich.pl>
2013-11-18 09:20:10 +0100

    Fix buttons

--- source/ui/buttons/airportdetailsbutton.cpp
+++ source/ui/buttons/airportdetailsbutton.cpp
@@ -16,6 +16,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <QtGui>
+
 #include "ui/windows/airportdetailswindow.h"
 
 #include "vatsimdata/airport.h"
