f145a1348e13ebcd33a4ab2cd959e0b23263a91c
Michał Garapich <michal@garapich.pl>
2014-09-21 19:25:58 +0200

    Init files resource

--- source/main.cpp
+++ source/main.cpp
@@ -34,6 +34,7 @@ QVariant lonLatInterpolator(const LonLat& start, const LonLat& end, qreal progre
 }
 
 int main(int argc, char** argv) {
+  Q_INIT_RESOURCE(files);
   Q_INIT_RESOURCE(imgs);
   Q_INIT_RESOURCE(shaders);
   Q_INIT_RESOURCE(weather);
